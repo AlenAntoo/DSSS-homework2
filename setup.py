dcbb6298aae6c2ba357716a53eef068dda8be99e
Alen Jose Anto <alen.anto@fau.de>
2024-11-07 14:28:38 +0100

    Made repository pip installable by adding setup.py and other files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    description="A simple math quiz game",
+    author="Alen Jose Anto",
+    author_email="alen.anto@fau.de",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here, if any
+)
